4031af0c98ec079ff5ca9604a03848306a2e87a9
Ryan Achten <ryan.achten@akqa.com>
2021-03-17 14:28:48 +1300

    Add plan model migration

---
+++ api/Migrations/20210317012743_UpdatePlantModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Seedling.Migrations
+{
+    public partial class UpdatePlantModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastWatered",
+                table: "Plants",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "WateringFrequency",
+                table: "Plants",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WateringPriod",
+                table: "Plants",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastWatered",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "WateringFrequency",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "WateringPriod",
+                table: "Plants");
+        }
+    }
+}
