52a9d70b351ce3946326dc46b3ae0c8428812a43
RyanAchten <ryanachten@gmail.com>
2021-03-19 11:17:49 +1300

    Integrate lightbox modal into media gallery

--- app/components/MediaGallery.tsx
+++ app/components/MediaGallery.tsx
@@ -1,8 +1,13 @@
-import { Button } from "@ui-kitten/components";
+import { Button, Modal } from "@ui-kitten/components";
 import React, { useState } from "react";
+import Lightbox from "react-native-image-gallery";
 import { Image, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Text } from "@ui-kitten/components";
 import { BiodiversityRecordMedia } from "../constants/Interfaces";
 import { Icon } from "./Icon";
+import { ModalBackground } from "../constants/Colors";
+import { Margin } from "../constants/Sizes";
 
 interface Props {
   media: Array<BiodiversityRecordMedia>;
@@ -17,30 +22,36 @@ export const MediaGallery = ({
 }: Props) => {
   const [errorIndices, setErrorIndices] = useState<Array<number>>([]);
   const [showAllImages, setShowAllImages] = useState(false);
+  const [showLightbox, setShowLightbox] = useState(false);
 
   if (!media || !media.length) {
     return <View />;
   }
 
   let mediaToShow = media.filter((el, i) => !errorIndices.includes(i));
-  if (!showAllImages) {
+  if (!showAllImages && !showLightbox) {
     mediaToShow = mediaToShow.slice(0, defaultImageCount);
   }
 
   return (
     <View style={[styles.imageWrapper, customStyles]}>
       {mediaToShow.map((m, i) => (
-        <Image
+        <TouchableOpacity
+          style={styles.imageThumbWrapper}
           key={i}
-          style={styles.imageThumb}
-          source={{
-            uri: m.identifier,
-          }}
-          onError={() => setErrorIndices([...errorIndices, i])}
-        />
+          onPress={() => setShowLightbox(true)}
+        >
+          <Image
+            style={styles.imageThumb}
+            source={{
+              uri: m.identifier,
+            }}
+            onError={() => setErrorIndices([...errorIndices, i])}
+          />
+        </TouchableOpacity>
       ))}
       <Button
-        style={styles.imageThumb}
+        style={styles.imageThumbWrapper}
         appearance="ghost"
         status="primary"
         accessoryLeft={() => (
@@ -55,18 +66,50 @@ export const MediaGallery = ({
       >
         {showAllImages ? "Show less" : "Show all"}
       </Button>
+      <Modal visible={showLightbox}>
+        <Button
+          onPress={() => setShowLightbox(false)}
+          style={styles.lightboxCloseButton}
+          accessoryLeft={() => (
+            <Icon fill="white" size="sm" name="close-circle-outline" />
+          )}
+        >
+          Close
+        </Button>
+        <Lightbox
+          style={styles.backdrop}
+          images={mediaToShow.map((m) => ({
+            source: {
+              uri: m.identifier,
+            },
+          }))}
+        />
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  backdrop: {
+    backgroundColor: ModalBackground,
+  },
   imageWrapper: {
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
   },
+  imageThumbWrapper: {
+    minWidth: "33.33%",
+    minHeight: 100,
+  },
   imageThumb: {
-    width: "33.33%",
+    width: "100%",
     height: 100,
+  },
+  lightboxCloseButton: {
+    marginTop: 100,
+    position: "absolute",
+    right: Margin.md,
+    zIndex: 1,
   },
 });
