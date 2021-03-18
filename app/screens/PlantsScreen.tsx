86f5369b313cce1d15b2bcf90c63c604259cdeb8
RyanAchten <ryanachten@gmail.com>
2021-03-18 15:17:43 +1300

    Update create plant button

--- app/screens/PlantsScreen.tsx
+++ app/screens/PlantsScreen.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { RefreshControl, StyleSheet } from "react-native";
+import { RefreshControl, StyleSheet, ScrollView } from "react-native";
 import { Text } from "@ui-kitten/components";
 import { Background, Button, ErrorToast } from "../components";
 import { ModalBackground } from "../constants/Colors";
 import { useNavigation } from "@react-navigation/native";
 import { PlantContext } from "../services/context";
 import { useContext } from "react";
-import { ScrollView } from "react-native-gesture-handler";
 
 export default function PlantScreen() {
   const nav = useNavigation();
@@ -27,7 +26,7 @@ export default function PlantScreen() {
   const goToEditScreen = () => nav.navigate("EditPlantScreen");
   return (
     <Background style={styles.container}>
-      <Button onPress={goToEditScreen}>Create</Button>
+      <Button onPress={goToEditScreen}>Create Plant</Button>
       <ScrollView
         refreshControl={
           <RefreshControl refreshing={loading} onRefresh={getPlants} />
