74f4e65bc4f50d0be3b0a54f144c8391439c7e02
RyanAchten <ryanachten@gmail.com>
2021-03-20 09:34:08 +1300

    Refactor plant media gallery to use react-native-snap-carousel

--- app/components/MediaGallery.tsx
+++ app/components/MediaGallery.tsx
@@ -1,115 +1,65 @@
-import { Button, Modal } from "@ui-kitten/components";
 import React, { useState } from "react";
-import Lightbox from "react-native-image-gallery";
-import { Image, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { Text } from "@ui-kitten/components";
+import Carousel from "react-native-snap-carousel";
+import {
+  Image,
+  StyleProp,
+  useWindowDimensions,
+  View,
+  ViewStyle,
+} from "react-native";
 import { BiodiversityRecordMedia } from "../constants/Interfaces";
-import { Icon } from "./Icon";
-import { ModalBackground } from "../constants/Colors";
-import { Margin } from "../constants/Sizes";
 
 interface Props {
   media: Array<BiodiversityRecordMedia>;
-  defaultImageCount?: number;
   styles?: StyleProp<ViewStyle>;
 }
 
-export const MediaGallery = ({
-  defaultImageCount = 5,
-  media,
-  styles: customStyles,
-}: Props) => {
-  const [errorIndices, setErrorIndices] = useState<Array<number>>([]);
-  const [showAllImages, setShowAllImages] = useState(false);
-  const [showLightbox, setShowLightbox] = useState(false);
+export const MediaGallery = ({ media, styles }: Props) => {
+  const [activeIndex, setActiveIndex] = useState<number>();
+  const [mediaToShow, setMediaToShow] = useState(media);
 
   if (!media || !media.length) {
     return <View />;
   }
 
-  let mediaToShow = media.filter((el, i) => !errorIndices.includes(i));
-  if (!showAllImages && !showLightbox) {
-    mediaToShow = mediaToShow.slice(0, defaultImageCount);
-  }
+  const { width } = useWindowDimensions();
+  const itemWidth = width - 200;
 
   return (
-    <View style={[styles.imageWrapper, customStyles]}>
-      {mediaToShow.map((m, i) => (
-        <TouchableOpacity
-          style={styles.imageThumbWrapper}
-          key={i}
-          onPress={() => setShowLightbox(true)}
-        >
-          <Image
-            style={styles.imageThumb}
-            source={{
-              uri: m.identifier,
-            }}
-            onError={() => setErrorIndices([...errorIndices, i])}
-          />
-        </TouchableOpacity>
-      ))}
-      <Button
-        style={styles.imageThumbWrapper}
-        appearance="ghost"
-        status="primary"
-        accessoryLeft={() => (
-          <Icon
-            size="sm"
-            name={
-              showAllImages ? "minus-circle-outline" : "plus-circle-outline"
-            }
-          />
-        )}
-        onPress={() => setShowAllImages(!showAllImages)}
-      >
-        {showAllImages ? "Show less" : "Show all"}
-      </Button>
-      <Modal visible={showLightbox}>
-        <Button
-          onPress={() => setShowLightbox(false)}
-          style={styles.lightboxCloseButton}
-          accessoryLeft={() => (
-            <Icon fill="white" size="sm" name="close-circle-outline" />
-          )}
-        >
-          Close
-        </Button>
-        <Lightbox
-          style={styles.backdrop}
-          images={mediaToShow.map((m) => ({
-            source: {
-              uri: m.identifier,
-            },
-          }))}
-        />
-      </Modal>
+    <View style={styles}>
+      <Carousel
+        layout={"default"}
+        data={mediaToShow}
+        sliderWidth={width}
+        itemWidth={itemWidth}
+        loop={true}
+        renderItem={({
+          item,
+        }: {
+          item: BiodiversityRecordMedia;
+          index: number;
+        }) => {
+          return (
+            <Image
+              style={{
+                height: 250,
+                width: itemWidth,
+              }}
+              source={{
+                uri: item.identifier,
+              }}
+              onError={() =>
+                setMediaToShow(
+                  [...mediaToShow].filter(
+                    (m) => m.identifier !== item.identifier
+                  )
+                )
+              }
+            />
+          );
+        }}
+        onSnapToItem={(index) => setActiveIndex(index)}
+      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  backdrop: {
-    backgroundColor: ModalBackground,
-  },
-  imageWrapper: {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
-  imageThumbWrapper: {
-    minWidth: "33.33%",
-    minHeight: 100,
-  },
-  imageThumb: {
-    width: "100%",
-    height: 100,
-  },
-  lightboxCloseButton: {
-    marginTop: 100,
-    position: "absolute",
-    right: Margin.md,
-    zIndex: 1,
-  },
-});
