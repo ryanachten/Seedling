7d80303e642d388d29cd99779adfc56bc8f6a678
RyanAchten <ryanachten@gmail.com>
2021-03-21 10:35:18 +1300

    Add calendar with markers for watering schedule

--- app/components/detail/InfoItem.tsx
+++ app/components/detail/InfoItem.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import {Text} from "@ui-kitten/components";
+import { Text } from "@ui-kitten/components";
 import { StyleSheet, View } from "react-native";
 import { Margin } from "../../constants/Sizes";
 
-export const InfoItem = ({ label, value }: { label: string; value: string }) => (
-    <View style={styles.infoItem}>
-      <Text category="label">{label}</Text>
-      <Text>{value}</Text>
-    </View>
-  );
+export const InfoItem = ({
+  label,
+  value,
+}: {
+  label: string;
+  value: string;
+}) => (
+  <View style={styles.infoItem}>
+    <Text category="label">{label}</Text>
+    <Text>{value}</Text>
+  </View>
+);
 
-  const styles = StyleSheet.create({
-    infoItem: {
-      marginBottom: Margin.sm,
-    },
-}
+const styles = StyleSheet.create({
+  infoItem: {
+    marginBottom: Margin.sm,
+  },
+});
--- app/components/detail/WateringScheduleCard.tsx
+++ app/components/detail/WateringScheduleCard.tsx
@@ -1,6 +1,14 @@
-import { formatDistance } from "date-fns";
+import {
+  addDays,
+  addMonths,
+  addWeeks,
+  addYears,
+  format,
+  formatDistance,
+} from "date-fns";
 import React from "react";
 import { View } from "react-native";
+import { Calendar, DotMarking } from "react-native-calendars";
 import { Plant, WateringPeriod } from "../../constants/Interfaces";
 import { InfoItem } from "./InfoItem";
 
@@ -8,20 +16,47 @@ interface Props {
   plant: Plant;
 }
 
+type CalendarMarker = { [date: string]: DotMarking };
+
 export const WateringScheduleCard = ({ plant }: Props) => {
+  const { lastWatered, wateringFrequency, wateringPeriod } = plant;
+  const markedDates = (): CalendarMarker => {
+    const dates = [new Date(lastWatered)];
+    let index = 0;
+    // Provide all watering dates for one year
+    while (dates[dates.length - 1] < addYears(Date.now(), 1)) {
+      switch (wateringPeriod) {
+        // Weeks
+        case 1:
+          dates.push(addWeeks(dates[index], wateringFrequency));
+          break;
+        // Months
+        case 1:
+          dates.push(addMonths(dates[index], wateringFrequency));
+          break;
+        // Days
+        case 0:
+        default:
+          dates.push(addDays(dates[index], wateringFrequency));
+      }
+      index++;
+    }
+    const dateMarkers: CalendarMarker = {};
+    const marker = { selected: true };
+    dates.map((d) => (dateMarkers[format(d, "yyyy-MM-dd")] = marker));
+    return dateMarkers;
+  };
   return (
     <View>
       <InfoItem
         label="Frequency"
-        value={`${plant.wateringFrequency} x ${
-          WateringPeriod[plant.wateringPeriod]
-        }`}
+        value={`${wateringFrequency} x ${WateringPeriod[wateringPeriod]}`}
       />
-      {plant.lastWatered && (
+      {lastWatered && (
         <InfoItem
           label="Last watered"
           value={`${formatDistance(
-            new Date(plant.lastWatered),
+            new Date(lastWatered),
             new Date(Date.now()),
             {
               addSuffix: true,
@@ -29,6 +64,7 @@ export const WateringScheduleCard = ({ plant }: Props) => {
           )}`}
         />
       )}
+      <Calendar markedDates={markedDates()} />
     </View>
   );
 };
